ba85095a06c57d39e45e4afc50c5a0a2c220d19d
donghae <choi7gold@gmail.com>
2025-12-22 16:36:03 +0900

    Notification 코드 추가, 수정

--- src/main/java/web/mvc/santa_backend/chat/controller/NotificationController.java
+++ src/main/java/web/mvc/santa_backend/chat/controller/NotificationController.java
@@ -6,9 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import web.mvc.santa_backend.chat.dto.NotificationDTO;
 import web.mvc.santa_backend.chat.service.NotificationService;
+import web.mvc.santa_backend.common.security.CustomUserDetails;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +19,22 @@ import web.mvc.santa_backend.chat.service.NotificationService;
 public class NotificationController {
     private final NotificationService notificationService;
 
-    @GetMapping("/api/notification/{userId}/{page}")
-    public ResponseEntity<?> getNotification(@PathVariable Long userId, @PathVariable int page){
+    /**
+     * 미구현
+     * @param page
+     * @param customUserDetails
+     * @return
+     */
+    @GetMapping("/api/notificationAll/{page}")
+    public ResponseEntity<?> getAllNotifications(@PathVariable Integer page, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        Long userId = customUserDetails.getUser().getUserId();
+        notificationService.getAllNotificationByUserId(userId, page);
+        return null;
+    }
+
+    @GetMapping("/api/notification/{page}")
+    public ResponseEntity<?> getNotification(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable int page){
+        Long userId = customUserDetails.getUser().getUserId();
         Page<NotificationDTO> notifications = notificationService.getNotificationByUserId(userId, page);
         return ResponseEntity.status(HttpStatus.OK).body(notifications);
     }
@@ -27,6 +43,12 @@ public class NotificationController {
     public ResponseEntity<?> createNotification(@RequestBody NotificationDTO notification){
         notificationService.createNotification(notification);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/api/notification")
+    public ResponseEntity<?> checkAllNotification(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        Long Id = customUserDetails.getUser().getUserId();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/api/notification/{notificationId}")
--- src/main/java/web/mvc/santa_backend/chat/service/NotificationService.java
+++ src/main/java/web/mvc/santa_backend/chat/service/NotificationService.java
@@ -8,12 +8,6 @@ import java.util.List;
 public interface NotificationService {
 
     /**
-     * 관리자..에서 사용할 지 몰라서 지우진 않았으나 아직 구현되지 않은 메서드
-     * @return
-     */
-    public List<NotificationDTO> getAllNotifications();
-
-    /**
      * user가 읽지 않은 알림만 가져오는 메서드
      * @param id
      * @param page
@@ -48,4 +42,6 @@ public interface NotificationService {
      * @param id
      */
     public void deleteNotificationById(Long id);
+
+    public void deleteAllNotificationById(Long userId);
 }
