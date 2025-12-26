af84d8e7d7175304669963b6a82e3e55aeea94be
SeokYong <132437309+Yoon0917@users.noreply.github.com>
2025-12-26 12:10:32 +0900

    Update RedisConfig.java

--- src/main/java/web/mvc/santa_backend/common/config/RedisConfig.java
+++ src/main/java/web/mvc/santa_backend/common/config/RedisConfig.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -32,9 +34,18 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+
+        // 2. 패스워드 설정 (RedisPassword 객체 사용)
+        config.setPassword(RedisPassword.of(password));
+
+        return new LettuceConnectionFactory(config);
     }
 
     /**
